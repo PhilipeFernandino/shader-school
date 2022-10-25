4734d0842fb6455fd599d9520d1fb5fc82b6bc83
PhilipeFernandino <philipegarciaf@gmail.com>
2022-10-25 17:13:32 -0300

    ok

--- 06-intro-6/mpow.glsl
+++ 06-intro-6/mpow.glsl
@@ -1,7 +1,11 @@
 mat2 matrixPower(highp mat2 m, int n) {
+  if (n == 0) return mat2(1.0);
   mat2 m2 = m;
-  for (int i = 0; i < 15; i++) 
-    m2 = m2 * m;
+  const int MAX_POWER = 16;
+  for (int i = 1; i < MAX_POWER; i++) {
+    if (i == n) break;
+    else m2 = m2 * m;
+  }
   return m2;  
 }
 
--- 08-frag-2/fragment.glsl
+++ 08-frag-2/fragment.glsl
@@ -1,8 +1,16 @@
 precision mediump float;
 
+#define WHITE vec4(1.0, 1.0, 1.0, 1.0)
+
+bool inTile(vec2 p, float tileSize) {
+  vec2 ptile = step(0.5, fract(0.5 * p / tileSize));
+  return ptile.x == ptile.y;
+}
+
 void main() {
 
-  //TODO: Replace this with a function that draws a checkerboard
-  
-  discard;
+  if (inTile(gl_FragCoord.xy, 16.0))
+    gl_FragColor = WHITE;
+  else
+    discard;
 }
